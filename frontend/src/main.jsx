60747be7508fcb975c8fc0de287851a9c000b94a
MenossiJose <jantoniomenossi@gmail.com>
2024-11-25 22:50:31 -0300

    changed routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./providers/UserContext.jsx";
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 )
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import  Login from "../pages/Login";
+import Login from "../pages/login/Login";
 
 const RoutesMain = () => {
-    return (
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    );
-  };
-  
-  export default RoutesMain;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  );
+};
+
+export default RoutesMain;
