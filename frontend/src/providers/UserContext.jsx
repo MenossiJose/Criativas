5088434a8e0933dd544f9b8d44d54d04c78564bd
MenossiJose <jantoniomenossi@gmail.com>
2024-11-28 20:02:29 -0300

    added register routes

--- frontend/src/providers/UserContext.jsx
+++ frontend/src/providers/UserContext.jsx
@@ -12,10 +12,12 @@ export const UserProvider = ({ children }) => {
     const navigate = useNavigate();
     const toast = useToast();
     const [loadingLogin, setLoadingLogin] = useState(false);
+    const [loadingRegister, setLoadingRegister] = useState(false);
 
     const userLogin = async (formData) => {
         try {
             setLoadingLogin(true);
+            //Alterar paraa rota correta
             const response = await api.post("/api/login/", formData);
             window.localStorage.clear();
             window.localStorage.setItem("@TOKEN", response.data.access);
@@ -41,8 +43,42 @@ export const UserProvider = ({ children }) => {
         }
     };
 
+    const userRegister = async (formData) => {
+        try {
+            setLoadingRegister(true);
+            //Alterar paraa rota correta
+            const response = await api.post("/api/register/", formData);
+            window.localStorage.clear();
+            window.localStorage.setItem("@TOKEN", response.data.access);
+            toast({
+                title: "Cadastro efetuado com sucesso!",
+                position: "top-right",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+            });
+            navigate("/home");
+        } catch (error) {
+            toast({
+                title: "Erro ao cadastrar!",
+                position: "top-right",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+            });
+            console.log(error);
+        } finally {
+            setLoadingRegister(false);
+        }
+    }
+
     return (
-        <UserContext.Provider value={{ userLogin, loadingLogin }}>
+        <UserContext.Provider value={{
+            userLogin,
+            loadingLogin,
+            loadingRegister,
+            userRegister
+        }}>
             {children}
         </UserContext.Provider>
     );
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
 
 const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
