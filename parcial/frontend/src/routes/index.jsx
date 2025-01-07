40ba628b192801493fbe941af5c21fcac373d802
MenossiJose <jantoniomenossi@gmail.com>
2025-01-07 17:25:14 -0300

    altered home

--- parcial/frontend/src/routes/index.jsx
+++ parcial/frontend/src/routes/index.jsx
@@ -2,13 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Auth from "../pages/auth/page";
 import SingUp from "../pages/signup/page";
 import LandingPage from "../pages/landingPage/page";
-
+import Home from "../pages/home/page";
 const RoutesMain = () => {
     return (
         <Routes>
             <Route path="/" element={<Auth />} />
             <Route path="/signup" element={<SingUp />} />
             <Route path="/landing" element={<LandingPage />} />
+            <Route path="/home" element={<Home />} />
         </Routes>
     );
 };
