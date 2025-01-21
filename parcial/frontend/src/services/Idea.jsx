b923fe1c77225fbd906f8e59de0afae7166796bf
MenossiJose <jantoniomenossi@gmail.com>
2025-01-21 16:23:16 -0300

    ideia creation fully functional

--- parcial/frontend/src/services/Idea.jsx
+++ parcial/frontend/src/services/Idea.jsx
@@ -5,28 +5,26 @@ export default function ideasServices() {
 
     const url = 'http://localhost:3000/ideas'
 
-    const getUsersIdeas = (userId) => {
-        setIdeasLoading(true)
 
-        fetch(`${url}/userIdeas/${userId}`, {
+
+    const getUsersIdeas = (userId) => {
+        setIdeasLoading(true);
+        return fetch(`${url}/userIdeas/${userId}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
+                'Access-Control-Allow-Origin': '*',
             },
         })
             .then((response) => response.json())
-            .then((result) => {
-                console.log(result)
-
-            })
             .catch((error) => {
-                console.log(error)
+                console.error(error);
+                return [];
             })
             .finally(() => {
-                setIdeasLoading(false)
-            })
-    }
+                setIdeasLoading(false);
+            });
+    };
 
     const createIdea = async (formData) => {
         setIdeasLoading(true)
@@ -79,6 +77,44 @@ export default function ideasServices() {
             });
     };
 
+    const likeIdea = (id, user) => {
+        return fetch(`${url}/${id}/like`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify({ id, user }), // Inclui o ID do usuário no corpo da requisição
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                return result;
+            })
+            .catch((error) => {
+                console.log(error);
+                return [];
+            });
+    };
 
-    return { ideasLoading, getUsersIdeas, createIdea, getAllIdeas }
+    const deleteIdea = async (ideaId) => {
+        setIdeasLoading(true);
+        try {
+            const response = await fetch(`${url}/${ideaId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+            });
+            return response.ok;
+        } catch (error) {
+            console.error('Error deleting idea:', error);
+            return false;
+        } finally {
+            setIdeasLoading(false);
+        }
+    };
+
+
+    return { ideasLoading, getUsersIdeas, createIdea, getAllIdeas, deleteIdea, likeIdea }
 }
